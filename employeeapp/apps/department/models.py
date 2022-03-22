9e7ecab1c316d36999e385f6852eb6486694e01c
codigoenlaweb <codeenlaweb@gmail.com>
2022-03-22 17:36:43 -0400

    model deparment

--- employeeapp/apps/department/models.py
+++ employeeapp/apps/department/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField("Department name",max_length=50)
+    short_name = models.CharField("department short name", max_length=20, unique=True)
+    anulate = models.BooleanField("department in operation", default=False)
+
+
+    class Meta:
+        verbose_name = "Deparment"
+        verbose_name_plural = "Deparments"
+
+
+    def activate_or_not(self) -> str:
+        return "department activate" if not self.anulate else "department desactivate"
+
+
+    def __str__(self) -> str:
+        return f'{self.short_name}: {self.activate_or_not()}'
--- employeeapp/employeeapp/urls.py
+++ employeeapp/employeeapp/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.home.urls')),
-    path('employee', include('apps.employee.urls')),
+    path('chulo', include('apps.employee.urls')),
     path('department', include('apps.department.urls')),
 ]
